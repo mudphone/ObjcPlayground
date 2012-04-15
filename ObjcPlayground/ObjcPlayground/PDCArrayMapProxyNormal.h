022d40a5bf396a785bcf8c0174e303175074497f
Kyle Oba <koba@mudphone.com>
2012-04-15 06:15:01 -1000

    Add junk dir.

---
+++ ObjcPlayground/ObjcPlayground/PDCArrayMapProxyNormal.h
@@ -0,0 +1,13 @@
+//
+//  PDCArrayMapProxyNormal.h
+//  ObjcPlayground
+//
+//  Created by Kyle Oba on 4/10/12.
+//  Copyright (c) 2012 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PDCArrayMapProxyNormal : NSProxy
+
+@end
---
+++ ObjcPlayground/ObjcPlayground/PDCArrayMapProxyNormal.m
@@ -0,0 +1,13 @@
+//
+//  PDCArrayMapProxyNormal.m
+//  ObjcPlayground
+//
+//  Created by Kyle Oba on 4/10/12.
+//  Copyright (c) 2012 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import "PDCArrayMapProxyNormal.h"
+
+@implementation PDCArrayMapProxyNormal
+
+@end
